24b3282565cefb4150b0bee76c0412a3fc29ca78
Dustin Gulley <dustingulley@Dustins-MacBook-Pro.local>
2020-06-08 01:28:18 -0400

    Added home buttons

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import Card from '@material-ui/core/Card';
-//import CardActions from '@material-ui/core/CardActions';
+import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
-//import Button from '@material-ui/core/Button';
+import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 
 const styles = {
@@ -10,19 +10,26 @@ const styles = {
         width: '80%',
         marginLeft: 'auto',
         marginRight: 'auto',
-        marginTop: '2em'
-    },
-    bullet: {
-        display: 'inline-block',
-        margin: '0 2px',
-        transform: 'scale(0.8)',
+        marginTop: '12%',
+        backgroundColor: '#FCEEFF',
     },
     title: {
         fontSize: '2em',
     },
-    pos: {
-        marginBottom: 12,
+    secondary: {
+        fontSize: '1em',
+        color: 'dimgray'
     },
+    buttons: {
+        marginTop: '1em',
+        textAlign: 'center',
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        width: '63%',
+    },
+    button: {
+        fontSize: '2em',
+    }
 };
 
 class Home extends React.Component {
@@ -35,9 +42,18 @@ class Home extends React.Component {
         return (
             <Card style={styles.root}>
                 <CardContent>
-                <Typography style={styles.title} color="textSecondary" gutterBottom>
-                    Welcome to Tic-Tac Paddy Whack!
-                </Typography>
+                    <Typography style={styles.title}>
+                        <strong>Welcome to Tic-Tac Paddy Whack!</strong>
+                    </Typography>
+                    <Typography style={styles.secondary}>
+                        A variation of tic-tac-toe in which you "paddy whack" an opponent by deleting one of their moves each turn after four moves
+                    </Typography>
+                    <div style={styles.buttons}>
+                        <CardActions>
+                            <Button style={styles.button} variant="contained" color="primary" href="create_game">Start a Game</Button>
+                            <Button style={styles.button} variant="contained" color="secondary" href="choose_game">Find a Game</Button>
+                        </CardActions>
+                    </div>
                 </CardContent>
             </Card>
         );
